30060827026d951006b9669e03b7d8d85fb54f30
Akata Arafat <haraphat@gmail.com>
2021-06-24 23:06:19 +0100

    Just finished with confirmable

--- db/migrate/20210624205232_add_confirmable_to_devise.rb
+++
@@ -1,4 +0,0 @@
-class AddConfirmableToDevise < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_205613) do
+ActiveRecord::Schema.define(version: 2021_06_24_210506) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -72,6 +72,11 @@ ActiveRecord::Schema.define(version: 2021_06_13_205613) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
