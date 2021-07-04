b82c07138146c2349c8170f26e16be3f46d2eb06
Akata Arafat <haraphat@gmail.com>
2021-07-04 22:24:15 +0100

    Used rolify gem to add roles to the users of the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def index
     @users = User.all.order(created_at: :desc)
     # @q = User.ransack(params[:q])
     # @users = @q.result(distinct: true)
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User #{@user.email} was updated usccesfully"
+    else
+      render :edit
+    end
+  end
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({role_ids:[]})
+  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'home/activity'
   devise_for :users
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
   resources :courses
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
